674b7cdb3f284da7a53a9d613337296357a29c43
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-18 15:33:23 +0200

    Project is almost done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel: (389) 79 270 586">(389) 79 270 586</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:hristijangorgioski503@gmail.com">hristijangorgioski503@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+          <SocialContainer>
+            <SocialIcons href="https://github.com/HristijanGjorgjioski">
+              <AiFillGithub size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="https://linkedin.com/">
+              <AiFillLinkedin size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="https://instagram.com/">
+              <AiFillInstagram size="3rem" />
+            </SocialIcons>
+          </SocialContainer>
+        </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
